142448b92734663424d38108dc4b3f659b84bca4
jeffreycoen <jeff.coen@gmail.com>
2017-12-12 10:23:59 -0500

    preliminary inclusion of bootstap.  verified to still work correctly.  hopefully this helps us later with the layout of the page

--- templates/jsFiles/treeLogic.js
+++ templates/jsFiles/treeLogic.js
@@ -1,79 +1,85 @@
+// Define the dataset
+var booksIReadThisYear = [12, 8, 7, 16, 2, 4, 11];
 
+var namesArray = [
+  "Abbott",
+  "Barney",
+  "Costello",
+  "Daisy",
+  "Edward",
+  "Fred",
+  "Georgia"
+];
 
-// set the dimensions of the canvas
-// var margin = {top: 600, right: 20, bottom: 70, left: 40},
-//     width = 600 - margin.left - margin.right,
-//     height = 300 - margin.top - margin.bottom;
+// Store the dimensions of the SVG container
+var svgWidth = 800;
+var svgHeight = 600;
 
+// Define the margins of the chart
+var margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+// Calculate the dimensions of the chart by subtracting the margin on either side from the width and height of the SVG container
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 
-var y = d3.scale.linear().range([height, 0]);
+// Define a bandScale which will be used to create our bottom axis and position and size our bars along the x-axis
+var xBandScale = d3
+  .scaleBand()
+  .domain(namesArray)
+  .range([0, chartWidth])
+  .padding(0.1);
 
-// define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
+// Define a linearScale which will be used to create our left axis and position and size our bars along the y-axis
+var yLinearScale = d3.scaleLinear().domain([0, 20]).range([chartHeight, 0]);
 
+// Configure a bottom axis function using the d3.axisBottom method and the band scale.
+var bottomAxis = d3.axisBottom(xBandScale);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+// Configure a left axis function using the d3.axisBottom method and the band scale.
+var leftAxis = d3.axisLeft(yLinearScale);
 
-
-// add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+// Create, size, and position the SVG container and SVG group which will hold the chart
+var svg = d3
+  .select("area2")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth)
   .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("height", chartHeight)
+  .attr("width", chartWidth)
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Append another SVG group and call the bottomAxis function inside
+svg
+  .append("g")
+  .attr("transform", "translate(0," + chartHeight + ")")
+  .call(bottomAxis);
 
-// load the data
-d3.json("https://raw.githubusercontent.com/AbrahamEapen/Meteorite-Market/master/templates/meteorites.json", function(error, data) {
+// Append another SVG group and call the leftAxis function inside
+svg.append("g").call(leftAxis);
 
-    data.forEach(function(d) {
-        d.recclass = d.recclass;
-        parseInt(d.mass) = +parseInt(d.mass);
-    });
-	
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.recclass; }));
-  y.domain([600, d3.max(data, function(d) { return parseInt(d.mass); })]);
-
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 600)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text(parseInt(d.mass));
-
-
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.recclass); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(parseInt(d.mass)); })
-      .attr("height", function(d) { return height - y(parseInt(d.mass)); });
-
-});
+// Select all elements with the class "bar" (none on page load)
+// Bind the booksIReadThisYear dataset to the empty selection
+// Run the enter method, which prepares one DOM element for each piece of data with no selected element
+// Append one `rect` for each piece of data
+// Give each rect a class of bar
+// Set the x coordinate to be the value returned from passing the corresponding name into the bandScale function
+svg
+  .selectAll(".bar")
+  .data(booksIReadThisYear)
+  .enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x", function(data, index) {
+    return xBandScale(namesArray[index]);
+  })
+  // Set the y coordinate to be the value returned from passing the data (current books value) into the linearScale function
+  .attr("y", function(data) {
+    return yLinearScale(data);
+  })
+  // Set the width of the bar using the bandWidth method attached to the bandScale function
+  .attr("width", xBandScale.bandwidth())
+  // Set the height of the bar to be the height of the chart minus the size calculated by the linear scale method
+  .attr("height", function(data) {
+    return chartHeight - yLinearScale(data);
+  });
